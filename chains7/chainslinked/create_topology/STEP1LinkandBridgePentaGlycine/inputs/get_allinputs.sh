f8c66d809d7cc19ac5eec14c627582a2f414c9da
kga-lab <kga.lab.iisc@gmail.com>
2026-07-17 18:27:42 +0530

    Add files via upload

---
+++ chains7/chainslinked/create_topology/STEP1LinkandBridgePentaGlycine/inputs/get_allinputs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp ../../../Linking/newresidue* .
+
+cp ../../../Linking/newbonds_ordered.dat .
+
+cp ../../../Linking/number_residues.tex .
+
+cp ../../../Linking/newreport.tex .
+
+cp ../../../../Freesimu/create_topology/STEP1/inputs/allchains.tex .
+
+cp ../../../../Freesimu/create_topology/STEP1/inputs/NAG_NAM_periodiclinks.tex .
---
+++ chains7/chainslinked/create_topology/STEP1LinkandBridgePentaGlycine/inputs/mvscript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+for i in {1..7}
+do
+mv newresidues_chain$i.tex residues_chain$i.tex
+done
